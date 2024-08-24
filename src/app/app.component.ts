13ec85964c649d22454b61c9a269bdeb8158c57f
RenanCruz7 <renancruz199@gmail.com>
2024-08-24 14:59:43 -0300

    Começando section principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { BottomBarComponent } from "./components/bottom-bar/bottom-bar.component";
+import { MainSectionComponent } from "./components/main-section/main-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent, BottomBarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, BottomBarComponent, MainSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
